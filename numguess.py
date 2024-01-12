af1cb4c9aaff7cfae4886c92a409ec67fd2403e3
bvince21 <124097099+bvince21@users.noreply.github.com>
2024-01-11 16:29:39 -0800

    Update numguess.py

--- numguess.py
+++ numguess.py
@@ -5,9 +5,9 @@ while(1):
  user=int(input("Guess the number which is btwn 1 and 100 : "))
  if user>=1 and user<=100:
      if user>num:
-         print("High")
+         print("Guess is to High")
      elif user<num:
-         print("Low")
+         print("Guess is to Low")
      else:
          print("CONGRATULATIONS!!! You Guessed The Number")
          exit(0)
